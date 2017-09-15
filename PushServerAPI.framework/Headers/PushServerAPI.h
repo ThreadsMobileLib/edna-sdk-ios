7af22b6399504a6e8814d86fd5b00db439d571b1
maslennikov <maslennikov@brooma.ru>
2017-09-15 14:39:59 +0700

    PushServerAPI lib addded

---
+++ PushServerAPI.framework/Headers/PushServerAPI.h
@@ -0,0 +1,18 @@
+//
+//  PushAPI.h
+//  PushAPI
+//
+//  Copyright © 2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PushAPI.
+FOUNDATION_EXPORT double PushServerAPIVersionNumber;
+
+//! Project version string for PushAPI.
+FOUNDATION_EXPORT const unsigned char PushServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushAPI/PublicHeader.h>
+
+
