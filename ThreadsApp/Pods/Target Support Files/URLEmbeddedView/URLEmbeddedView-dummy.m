dd2290753d9adeb9c16d8511769ae8b38a5d8503
maslennikov <maslennikov@brooma.ru>
2019-04-26 16:27:57 +0700

    Release 2.48.0.2

--- ThreadsApp/Pods/Target Support Files/URLEmbeddedView/URLEmbeddedView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_URLEmbeddedView : NSObject
-@end
-@implementation PodsDummy_URLEmbeddedView
-@end
--- ThreadsApp/ThreadsApp/View/About/CounterListener.swift
+++ ThreadsApp/ThreadsApp/View/About/CounterListener.swift
@@ -16,5 +16,32 @@ import Threads
         Threads.threads().unreadedMessagesCountChanged = { (messagesCount) in
             print("SWIFT COUNTER LISTENER, count = ", messagesCount);
         }
+        
+        //Check Swift version
+        #if swift(>=5.0)
+            print("Hello, Swift 5.0")
+        #elseif swift(>=4.2)
+            print("Hello, Swift 4.2")
+        #elseif swift(>=4.1)
+            print("Hello, Swift 4.1")
+        #elseif swift(>=4.0)
+            print("Hello, Swift 4.0")
+        #elseif swift(>=3.2)
+            print("Hello, Swift 3.2")
+        #elseif swift(>=3.0)
+            print("Hello, Swift 3.0")
+        #elseif swift(>=2.2)
+            print("Hello, Swift 2.2")
+        #elseif swift(>=2.1)
+            print("Hello, Swift 2.1")
+        #elseif swift(>=2.0)
+            print("Hello, Swift 2.0")
+        #elseif swift(>=1.2)
+            print("Hello, Swift 1.2")
+        #elseif swift(>=1.1)
+            print("Hello, Swift 1.1")
+        #elseif swift(>=1.0)
+            print("Hello, Swift 1.0")
+        #endif
     }
 }
