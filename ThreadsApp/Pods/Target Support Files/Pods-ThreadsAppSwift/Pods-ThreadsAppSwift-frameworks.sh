c584f328a6ccb960138cfe6c2c61990e61586524
vitkuzmenko <kuzmenko.v.u@gmail.com>
2019-07-17 09:34:14 +0300

    Removed ReactiveObjC dependency

--- ThreadsApp/Pods/Target Support Files/Pods-ThreadsAppSwift/Pods-ThreadsAppSwift-frameworks.sh
+++ ThreadsApp/Pods/Target Support Files/Pods-ThreadsAppSwift/Pods-ThreadsAppSwift-frameworks.sh
@@ -154,11 +154,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MMDrawerController/MMDrawerController.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MMDrawerController/MMDrawerController.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
