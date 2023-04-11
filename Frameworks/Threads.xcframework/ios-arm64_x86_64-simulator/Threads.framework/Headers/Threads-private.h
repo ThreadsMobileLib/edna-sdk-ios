21834da52af72abf8f4d4457a0a4980d8715f158
edna <edna-ios-bot@edna.ru>
2023-04-11 18:20:23 +0300

    Release 4.15.0

---
+++ Frameworks/Threads.xcframework/ios-arm64_x86_64-simulator/Threads.framework/Headers/Threads-private.h
@@ -0,0 +1,9 @@
+//
+//  Threads-private.h
+//  Threads
+//
+//  Copyright © 2020 edna. All rights reserved.
+//
+
+// Headers exposed to Swift via module Threads.Private
+
---
+++ Frameworks/Threads.xcframework/ios-arm64_x86_64-simulator/Threads.framework/Headers/Threads.h
@@ -0,0 +1,16 @@
+//
+//  Threads.h
+//  Threads
+//
+//  Copyright © 2022 edna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Threads.
+FOUNDATION_EXPORT double ThreadsVersionNumber;
+
+//! Project version string for Threads.
+FOUNDATION_EXPORT const unsigned char ThreadsVersionString[];
+
+
